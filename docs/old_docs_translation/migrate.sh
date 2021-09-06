aa97cf8eb0e5612911d9131e12c744d6bce3e6bd
Matthias Kuhn <matthias@opengis.ch>
2021-09-06 23:21:06 +0200

    fix translation path

--- docs/old_docs_translation/migrate.sh
+++ docs/old_docs_translation/migrate.sh
@@ -3,8 +3,8 @@
 for lang in i18n/*; do
   lang=${lang%*/}
   lang=${lang##*/}
-  for po in i18n/$lang/*.po; do
+  for po in i18n/$lang/LC_MESSAGES/*.po; do
     fn=$(basename -- ${po%*.po})
-    msgfmt -o i18n/$lang/LC_MESSAGES/$fn.mo $po
+    msgfmt -o i18n/$lang/LC_MESSAGES/$fn.mo i18n/$lang/LC_MESSAGES/$fn.po
   done
 done
