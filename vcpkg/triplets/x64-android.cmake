0bab697f528ab76b0066d85c31cf82c9f6d6a1aa
Matthias Kuhn <matthias@opengis.ch>
2021-10-27 17:34:04 +0100

    Od

--- vcpkg/triplets/x64-android.cmake
+++ vcpkg/triplets/x64-android.cmake
@@ -3,6 +3,6 @@ set(VCPKG_CRT_LINKAGE static)
 set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_CMAKE_SYSTEM_NAME Android)
 
-if(PORT STREQUAL "libpq")
+if(PORT STREQUAL "openssl")
   set(VCPKG_LIBRARY_LINKAGE dynamic)
 endif()
